2c2a016cbb9ea31a8c140c7121233f08f2bb4c6b
EilafSSA <eyeeilaf@gmail.com>
2025-05-25 19:03:23 +0300

    small rabbit sprite test, going to add code later on
    "        //Animals plan!
            //Should be a rabbit/bunny, fish, duck (?), cat, dog, hamster,
            // maybe another fun animal like a baby tiger as a percentage by picking the cat and thne youll have to release the tiger after 5 minutes
            // like a wolf with the dog, a baby shark with the fish, a baby hare with the bunny and maybe with the hamster its a squirel.
            // The duck can be a goose... "
    
    Heres a small plan and added rabbit sprite

--- Tomogachi-ESS_Spring2025/obj/Debug/net9.0/Tomogachi-ESS_Spring2025.AssemblyInfo.cs
+++ Tomogachi-ESS_Spring2025/obj/Debug/net9.0/Tomogachi-ESS_Spring2025.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tomogachi-ESS_Spring2025")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5c24f11353100f4e0481ddc6e5a5d58dfb95183")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74caa96ee00e5eb2d7a5f13688f686b7e45a6155")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tomogachi-ESS_Spring2025")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tomogachi-ESS_Spring2025")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
